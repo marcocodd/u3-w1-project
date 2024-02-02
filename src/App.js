6beddef391ec9a7418024d8e7ad19bbcf999bc96
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-02-02 17:08:08 +0100

    mb 5 alle row

--- src/App.js
+++ src/App.js
@@ -20,17 +20,17 @@ function App() {
     </header>
     <main>
      <h4>Trending Now</h4>
-     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-4">
+     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-5">
       {/* immessa un'url errata per prova alert ho aggiunto un 1 subito dopo .com/ */}
 
       <FilmsGallery url="http://www.omdbapi.com/1?apikey=59396482&s=Ciao" />
      </Row>
      <h4>Watch it Again</h4>
-     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-4">
+     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-5">
       <FilmsGallery url="http://www.omdbapi.com/?apikey=59396482&s=Pixar" />
      </Row>
      <h4>New Releases</h4>
-     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-4">
+     <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 g-3 mb-5">
       <FilmsGallery url="http://www.omdbapi.com/?apikey=59396482&s=Action" />
      </Row>
     </main>
